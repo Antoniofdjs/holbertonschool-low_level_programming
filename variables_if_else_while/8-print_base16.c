5f7487ad469f580de7e531450bb1c6d096eff05d
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-16 15:29:44 -0400

    Put chars for sets of prints number and letter

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main- funcion del main entry
+ *
+ *
+ *Return: devolver 0
+ *
+*/
+int main(void)
+{
+	char letra = '0';
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar (letra);
+		letra++;
+		i++;
+	}
+	letra = 'a';
+	i = 0;
+	while (i < 7)
+	{
+		putchar (letra);
+		letra++;
+		i++;
+	}
+	return (0);
+}
