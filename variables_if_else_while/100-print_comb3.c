d568ffaff6ce00671d54c56528e0a30aacf8bd63
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-16 21:40:20 -0400

    Using 2 index, j and i for combinations

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,14 @@ int main(void)
 {
 	int num = 0;
 	int j = 0;
+
 	for (j = 0; j <= 9; j++)
 	{
 	for (num = j + 1; num <= 9; num++)
 	{
 		putchar('0' + j);
 		putchar ('0' + num);
-		if ( j != 8 || num != 9)
+		if (j != 8 || num != 9)
 		{
 			putchar(',');
 			putchar(' ');
