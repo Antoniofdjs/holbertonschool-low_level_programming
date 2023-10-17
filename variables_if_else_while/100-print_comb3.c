54ddd6dc8d67f2e1279c162c7d1853e85b265b61
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-16 21:32:51 -0400

    Using 2 index, j and i for combinations

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- funcion del main entry
+ *
+ *
+ *Return: devolver 0
+ *
+*/
+int main(void)
+{
+	int num = 0;
+	int j = 0;
+	for (j = 0; j <= 9; j++)
+	{
+	for (num = j + 1; num <= 9; num++)
+	{
+		putchar('0' + j);
+		putchar ('0' + num);
+		if ( j != 8 || num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
