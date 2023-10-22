91f3cd7138ad0581857b5611ea3aa2ebac4b3bea
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-21 22:58:52 -0400

    COmbinations of 00 01 ...

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0, j = i;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if ( i != j)
+			{
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
+				putchar(' ');
+				putchar('0' + j / 10);
+				putchar('0' + j % 10);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+}
+return (0);
+}
