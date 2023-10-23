40f83a8fe540b7b4d4ff7ba2a9e7bb88c7d577a9
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-23 15:33:35 -0400

    Reverse a string, putting it in the same string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *rev_string - reverses the string
+ *
+ *@s: stores string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	int end;
+	char temp[25];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	end = i;
+	while (i >= 0)
+	{
+		temp[j] = s[i];
+		i--;
+		j++;
+	}
+	i = 0;
+	j = 0;
+	while (i <= end)
+	{
+		s[i] = temp[j];
+		i++;
+		j++;
+	}
+
+}
