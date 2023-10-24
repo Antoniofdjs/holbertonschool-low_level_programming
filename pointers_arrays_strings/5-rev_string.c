443fca321217b2cf8ffd55c5f6b71c5cb005c4c5
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-23 20:47:25 -0400

    Reverse fix....

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,41 +1,43 @@
 #include "main.h"
 /**
- *rev_string - reverses the string
+ * rev_string - Reverses string
  *
- *@s: stores string
- *
+ * @s: stores string
  */
-
 void rev_string(char *s)
 {
-	int i = 0, j = 0;
+
+	int i = 0;
+	int j = 0;
 	int end;
 	char temp[10];
-
+	
 	while (s[i] != '\0')
+{
+	i++;
+}
+i = i - 1;
+end = i;
+
+while (i > j)
+{
+	if (s[i] != '0')
 	{
-		i++;
-	}
-	i = i - 1;
-	end = i;
-	while (i > j)
-	{
-		if (s[i] != '\0')
-		{
 		temp[j] = s[i];
-		}
-		i--;
-		j++;
+		temp[i] = s[j];
 	}
-	i = 0;
-	j = 0;
-	while (i <= end)
+	i--;
+	j++;
+}
+i = 0;
+j = 0;
+while (i <= end)
+{
+	if (temp[j] != '\0')
 	{
-		if (temp[j] != '\0')
-		{
-			s[i] = temp[j];
-		}
-		i++;
-		j++;
+		s[i] = temp[j];
 	}
+	i++;
+	j++;
+}
 }
