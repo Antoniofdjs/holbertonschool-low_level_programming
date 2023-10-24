b41abb4f789c02c18c09ca851275b6dbf79a5f4d
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-24 10:02:00 -0400

    copy a string when you have the pointers to that string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcpy - Prints half
+ *
+ *@src: points to string
+ *@dest: destination to copy src string
+ *Return: copy of the string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
