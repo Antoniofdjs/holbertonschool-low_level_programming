4b1c3bc0ba69eae02052b54426485be657f3e067
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-24 12:55:40 -0400

    atoi function recreation

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_atoi - takes strings of numbers an puts them in a int
+ *
+ *@s: stores string
+ * Return: int value of string text
+ */
+int _atoi(char *s)
+{
+	int j = 0, i = 0;
+	int signo = 1, end = 0;
+	int result = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j <= i; j++)
+	{
+		if ((s[j] >= '0' && s[j] <= '9'))
+		{
+			if (s[j - 1] == '-' && end == 0)
+			{
+				signo = -1;
+				end++;
+			}
+			result = result * 10 + (s[j] - '0');
+			if (s[j + 1] <= '0' || s[j + 1] >= '9')
+			{
+				j = i + 1;
+			}
+		}
+	}
+	result = result * signo;
+	return (result);
+}
