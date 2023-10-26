e95ef9747b6cda7634c2d462ec0802d0a3a74202
Antoniofdjs <Antoniofdjs@gmail.com>
2023-10-26 15:34:36 -0400

    Fix for 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,51 +1,27 @@
 #include "main.h"
 /**
-**_strstr - str_str function
-*
-*@needle: String to find inside another
-*@haystack: Original string
-*Return: a string or nul depending if is true
-*/
+ * _strstr - finds a substring
+ * @haystack: the string
+ * @needle: the substring
+ *
+ * Return: pointer to substring
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int j = 0;
-	int i = 0;
-	int k = 0, cutindex;
-	char *NUL = '\0';
+	while (*haystack)
+	{
+		char *x = haystack;
+		char *y = needle;
 
-	while (needle[i] != '\0')
-	{
-		i++;
-	}
-	while (haystack[k] != '\0')
-	{
-		while (j <= i)
+		while (*haystack == *y && *y != 0 && *haystack != 0)
 		{
-			if (needle[j] == haystack[k])
-			{
-				cutindex = k;
-				while (needle[j] == haystack[k])
-				{
-					j++;
-					k++;
-					if (j == i && needle[j] == haystack[k])
-					{
-						return (haystack + cutindex);
-					}
-					else if (j == i)
-					{
-						return (NUL);
-					}
-				}
-			}
-			else
-			{
-				j = 0;
-				k++;
-			}
-			j = 0;
+			haystack++;
+			y++;
 		}
+		if (*y == 0)
+			return (x);
+		haystack = ++x;
 	}
-	return (NUL);
+	return (0);
 }
