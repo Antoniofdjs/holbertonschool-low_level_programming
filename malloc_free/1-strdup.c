795bb7bbc74e7913eb5b5299fa3a8352f760d0a0
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-04 11:54:56 -0400

    Duplicating string fix for size

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,14 +9,14 @@
 char *_strdup(char *str)
 {
 	unsigned int size = strlen(str);
-	char *ptr = (char *)malloc(size * sizeof(char));
+	char *ptr = (char *)malloc((size + 1) * sizeof(char));
 	unsigned int i;
 
 	if ((ptr == NULL) || (size == 0))
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size + 1; i++)
 	{
 		ptr[i] = str[i];
 	}
