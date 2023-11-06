89cc08c436a39a600f4bb22b6cf497087b7d759a
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-06 12:49:25 -0400

    Fix if cases when s1 null or s2 null

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,10 +25,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		size2 = strlen(s2);
 	}
-	if (n < size2)
-	{
-		size2 = n;
-	}
 	if (n >= size2)
 		n = size2;
 	ptr = malloc(size1 + size2 + 1);
