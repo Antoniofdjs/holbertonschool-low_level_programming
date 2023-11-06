06036e2bf95373112b5f221481a330f90a8260ce
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-06 13:41:13 -0400

    Valgrind check

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,6 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
+	unsigned int i, j;
 	unsigned int size1 = 0, size2 = 0;
 
 	if (s1 != NULL)
@@ -26,10 +27,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc(size1 + size2 + 1);
 	if (ptr == NULL)
 		return (NULL);
-	if (s1 != NULL)
-		strcpy(ptr, s1);
-	if (s2 != NULL)
-		strncat(ptr, s2, n);
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < size2; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
 	return (ptr);
 
 }
