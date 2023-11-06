06f4a8c1ae22d1f48fe889762beb13d3d81ab97c
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-06 16:44:03 -0400

    Fixed betty

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2 = strlen(s2);
 	if (n < size2)
 		size2 = n;
-	ptr = malloc(size1 +size2 + 1);
+	ptr = malloc(size1 + size2 + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size1; i++)
