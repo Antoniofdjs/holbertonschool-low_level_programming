7e27d3ce08b93aecf3dda5d5216b17e2e3c464d6
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-07 23:15:54 -0400

    fix for betty

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,37 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ *argstostr - Concat argv
+ *
+ *@ac: argc
+ *@av: argv
+ *Return: pointer result
+ */
 char *argstostr(int ac, char **av)
 {
-    char *result;
-    int total_length = 0;
-    int index = 0;
-    int i, j;
+	char *result;
+	int total_length = 0;
+	int index = 0;
+	int i, j;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		total_length += strlen(av[i]);
+	result = malloc(total_length + ac + 1);
+	if (result == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++) {
-        total_length += strlen(av[i]);
-    }
-
-    result = malloc(total_length + ac + 1);
-    if (result == NULL)
-        return (NULL);
-
-    for (i = 0; i < ac; i++) {
-        for (j = 0; av[i][j] != '\0'; j++) {
-            result[index++] = av[i][j];
-        }
-
-        if (i < ac - 1) {
-            result[index++] = '\n';
-        } else {
-            result[index++] = '\n';
-        }
-    }
-
-    result[index] = '\0';
-    return result;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			result[index++] = av[i][j];
+		result[index++] = '\n';
+	}
+	result[index] = '\0';
+	return (result);
 }
