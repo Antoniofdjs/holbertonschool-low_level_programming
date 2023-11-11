0a1f0ae7d74613743f8716ff9d41e89013e1261f
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-11 00:55:16 -0400

    New struct member initialized

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog- function that creates new dog
+ *
+ * @name: dog name
+ * @age: dog's age
+ * @owner: name of owner
+ * Return: Struct of new member of the struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+
+	return (new_dog);
+}
