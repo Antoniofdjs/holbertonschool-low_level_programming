690ffb6325fe4118b6f65a569cbc44606e70aba5
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-10 21:29:00 -0400

    Fixed comments

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,16 @@
-/**
- * struct dog - Info about dog
- *@name: pointer to string, Dog's name
- *@age: float for age of dog
- *@owner: pointer to name of owner
-*/
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Info about dog
+ * @name: pointer to string, Dog's name
+ * @age: float for age of dog
+ * @owner: pointer to name of owner
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+  char *name;
+  float age;
+  char *owner;
 };
 
 #endif /* DOG_H */
