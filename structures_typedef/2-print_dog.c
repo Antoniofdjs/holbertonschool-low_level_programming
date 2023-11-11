d009177cc18ccf0dff7c83459cc8308b0ddbef17
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-10 23:15:44 -0400

    Fixed print cases

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,10 +8,15 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL || d->name == NULL || d->owner == NULL)
+	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n"
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
-
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
 }
