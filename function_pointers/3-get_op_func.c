b7b8d866665a0165195ec3f9d463b89e9c1c1f50
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-13 03:05:51 -0400

    Trying to fix core dump and prints i left

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,12 +18,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (i < 5)
 	{
-		printf("While indice %d\n", i);
-		if (*s == ops[i].op[0])
+		if (strcmp(s,ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,21 +14,21 @@ int main(int argc, char *argv[])
 	int num1 = 0, num2 = 0;
 	int result = 0;
 	char *operator;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2];
-	printf("Check right before get_op\n");
+
 	result = (*get_op_func(operator))(num1, num2);
-	
+
 	printf("%d\n", result);
-	
+
 	return (0);
 }
