d3de7c7f29b9b3243e22326a1ad3413fb9af2bbb
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-13 02:00:46 -0400

    Test calc #1

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * get_op_func- picks the function to perform operation asked by user.
+ * @s: operator passed as argument + - * / %
+ * Return: result of operation followed by new line
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].op != NULL)
+	{
+		printf("While indice %d\n", i);
+		if (strcmp(s,ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <strings.h>
+#include <stdlib.h>
+/**
+ * main- Prints result of the operation in command line
+ *
+ * @argc: argument count
+ * @argv: argment values
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = 0, num2 = 0;
+	int result = 0;
+	char *operator;
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	printf("Check right before get_op\n");
+	result = (*get_op_func(operator))(num1, num2);
+	
+	printf("%d\n", result);
+	
+	return (0);
+}
