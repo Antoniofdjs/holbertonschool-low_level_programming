6165d4fa014493ba574242a6a79ab5d4f5d25bf9
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-14 15:12:13 -0400

    Prints numbers and separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints all numbers including separator
+ * @n: number of ints passed
+ * @separator: string to be printed between numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list my_args;/* my variable for va list */
+
+	if (n == 0)
+		return;
+
+	va_start(my_args, n);/* initialize with n arguments */
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i != n - 1)/* Prints if not null and not last number */
+		{
+			if (i != n - 1)
+				printf("%d%s", va_arg(my_args, int), separator);
+		}
+		else
+			printf("%d", va_arg(my_args, int));/* Prints only the numbers */
+	}
+	va_end(my_args);/* End my va list */
+	printf("\n");
+}
