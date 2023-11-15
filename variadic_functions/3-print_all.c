b5b9460d7cf1d41f9bfcb034a8de040c138e6489
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-14 23:24:01 -0400

    Better comments

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -50,32 +50,34 @@ void print_all(const char * const format, ...)
 {
 	int i = 0, j = 0;
 	va_list my_args;
-	char *separator = "";
+	char *separator = ""; /*Prnt before prnts from functions,except last one */
 	data my_data[] = {
 		{"c", print_c},
 		{"i", print_i},
 		{"f", print_f},
 		{"s", print_s},
-		{NULL, NULL}
-	};
+		{NULL, NULL} /*letter-type, (function*) */
+	};/*Struct char *type, void (*f)()*/
 
 	va_start(my_args, format);
-	while (format != NULL && format[i] != '\0')
+
+	while (format != NULL && format[i] != '\0')/*lettrs exist in format,not null*/
 	{
 		j = 0;
-		while (my_data[j].f != NULL)
+		while (my_data[j].f != NULL)/*still have functions to call*/
 		{
-			if (*my_data[j].type == format[i])
+			if (*my_data[j].type == format[i])/*match letters = data type */
 			{
-				printf("%s", separator);
-				my_data[j].f(my_args);
-				separator = ", ";
+				printf("%s", separator);/*prnts before function, last print has no sep*/
+				my_data[j].f(my_args);/*call matching function */
+				separator = ", ";/*set value atfer 1st print*/
 				break;
 			}
 			j++;
 		}
 		i++;
 	}
+
 	va_end(my_args);
 	printf("\n");
 }
