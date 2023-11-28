cb3d15611d85fdb4f9173c95fda6a4db4a19221b
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-28 01:38:43 -0400

    used strlen instead of i, also removed copy_str

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,25 +12,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	char *copy_str = strdup(str);
-	int i = 0; /* Len of str */
 	list_t *new_node;
-
-	while (copy_str[i] != '\0')
-		i++; /* len of str */
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = copy_str;
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = i;
+	new_node->len = strlen(str);
 	new_node->next = *head;/*coges el adress del que era primer node */
 	*head = new_node;/* head ahora empieza en new node */
 
