e9156c0bba89a740a586f8562d4217b0156d007f
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-29 07:47:15 -0400

    add node to the start od Dlist

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint- Add to the start a Dlinked node
+ *
+ * @n: int from struct
+ * @head: pointer to nodes or null
+ * Return: Adress of new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	int copy_n;
+	dlistint_t *new_node;
+
+	copy_n = n;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		printf(" Memory failed to allocate\n");
+		return (NULL);
+	}
+
+	new_node->n = copy_n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+
+	return (new_node);
+}
