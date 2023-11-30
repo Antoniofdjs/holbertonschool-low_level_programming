920bf05354d95759422994160a27e4681276f56c
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-30 14:32:34 -0400

    Fixed while current != NULL)

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	current = *h;
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		if (count == idx)
 		{
