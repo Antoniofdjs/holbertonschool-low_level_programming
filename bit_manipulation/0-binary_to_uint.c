1fab222286b27bdb4668638293f14a05b2772e14
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-02 11:13:24 -0400

    fixed betty comments excces *

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,6 @@
 #include "main.h"
 
 /**
- *
  *binary_to_uint- convert binary str to unsigned int
  *
  *@b: str containing binary
@@ -16,14 +15,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sizelen = 0;
 
 	sizelen = strlen(b);
-	for (i = sizelen -1 ; i >= 0; i--)
+	for (i = sizelen - 1 ; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		
+
 		if (b[i] == '1')
 			total = total + dec_val;
-		
+
 		dec_val = dec_val * 2;
 	}
 	return (total);
