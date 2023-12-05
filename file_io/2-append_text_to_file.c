ccee7d8fa468516cb911a621c27c0713f5d12f00
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-05 00:47:47 -0400

    append to a file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - append text at the end of a file
+ * @filename: name of the file
+ * @text_content:  is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int letters = 0, fd;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	while (text_content[letters] != '\0') /* how many chars */
+	{
+		letters++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, letters);
+
+	return (1);
+}
