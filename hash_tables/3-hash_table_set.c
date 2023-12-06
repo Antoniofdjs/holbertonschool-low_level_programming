a5111dd47ef5fac052fd0249137285b5cb1f1745
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-06 12:11:03 -0400

    test 3 for checkers

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,17 +14,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int size = 0;
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current_node;
 
-	if (key == NULL || *key == '\0')
+	if (key == NULL || *key == '\0' || value == NULL)
 		return (-1);
 
 	size = ht->size;
 	index = (hash_djb2((const unsigned char *)key) % size);
 
-	new_node = malloc(sizeof(hash_node_t));
+	current_node = ht->array[index]; /* get current node from index */
+
+	while (current_node) /* check if key exists in list */
+	{
+		if (strcmp(current_node->key, key) == 0) /* match key, update new value */
+		{
+			current_node->value = strdup(value);
+			return (0);
+		}
+		current_node = current_node->next;/* next node */
+	}
+
+	new_node = malloc(sizeof(hash_node_t)); /* create new node*/
 	if (new_node == NULL)
 		return (-1);
+
+	/*
+	 * assign all values for node
+	 */
 
 	new_node->value = strdup(value);
 	new_node->key = strdup(key);
