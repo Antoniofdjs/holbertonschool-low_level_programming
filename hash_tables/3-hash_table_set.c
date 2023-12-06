cdc78afa2466341dcf5332ac243909ac51607676
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-06 10:34:13 -0400

    testing checkers 2

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,18 +17,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 
 	if (key == NULL || *key == '\0')
-		return (0);
+		return (-1);
 
 	size = ht->size;
 	index = (hash_djb2((const unsigned char *)key) % size);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (-1);
 
 	new_node->value = strdup(value);
 	new_node->key = strdup(key);
-	new_node = ht->array[index];
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
